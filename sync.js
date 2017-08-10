e20f379e0dbc43b0d40048b99f6625a0ca067cdd
royalgarter@gmail.com <royalgarter@gmail.com>
2017-08-10 13:30:24 +0700

    clean up, rename

--- sync.js
+++ sync.js
@@ -3,54 +3,69 @@
 module.exports = (fn, ... args) => { 
 	const cb = (args.length && typeof args[args.length-1] === 'function') 
 				? args[args.length-1] : null;
+
+	const isPromise = x => !!x && ~['object', 'function'].indexOf(typeof x)
+							&& typeof x.then === 'function' 
+							&& typeof x.catch === 'function';
 	
-	const finish = (err, result, iter) => (iter && iter.return()) & (cb && cb(err, result));
+	const finish = (error, result, iterator) => {
+		if (iterator) {
+			if (iterator.isFinished) return;
+
+			iterator.isFinished = true;
+			iterator.return();
+		}
+
+		return cb && cb(error, result);
+	}
 
 	const callAsync = (fnNArgs, callback) => {
 		const argsOnly = Array.prototype.slice.call(fnNArgs, 1);
 		argsOnly.push(callback);
-		
-		let obj = fnNArgs[0].apply(fnNArgs[0], argsOnly);
 
-		if (!!obj && (typeof obj === 'object' || typeof obj === 'function') 
-			&& typeof obj.then === 'function' && typeof obj.catch === 'function') {
-			// console.log('obj is Promise')
-			obj.then(pPesult => callback(null, pPesult)).catch(pError => callback(pError));
+		let tmp = fnNArgs[0].apply(fnNArgs[0], argsOnly);
+
+		if (isPromise(tmp)) {
+			// console.log('tmp is Promise')
+			tmp.then( pPesult => callback(null, pPesult) )
+				.catch( pError => callback(pError) );
 		}
-			
 	}
 
-	const obj = {
+	const TEMPLATE = {
 		generator: typeof fn === 'function' ? fn : null, 
 		breaker: {flag: false, err: null, data: null},
 		isTolerant: false,
+		isFinished: false,
+		isPromise: isPromise
 	};
 
-	if (typeof fn === 'object') Object.assign(obj, fn);
+	if (typeof fn === 'object') Object.assign(TEMPLATE, fn);
 
-	if (!obj.generator) return finish('EMISSGENERATOR');
+	if (!TEMPLATE.generator) return finish('EGENERATORMISSING');
 
-	const iter = obj.generator.apply(obj.generator, args);
-	Object.assign(iter, obj);
+	const _iter = TEMPLATE.generator.apply(TEMPLATE.generator, args);
+	Object.assign(_iter, TEMPLATE);
 
-	iter.break = (err, data) => iter.breaker = {flag: true, err: err, data: data};
+	_iter.break = (err, data) => _iter.breaker = {flag: true, err: err, data: data};
 
-	iter._loop = (err, data) => {
+	_iter._loop = (err, data) => {
 		if (err) {
-			if (iter.isTolerant) data = void 0;
-			else return cb ? finish(err, data, iter) : iter.throw(err);
+			if (_iter.isTolerant) data = void 0;
+			else return cb ? finish(err, data, _iter) : _iter.throw(err);
 		} 
 
-		const nextPart = iter.next(data);
+		const nextPart = _iter.next(data);
 
-		if (iter.breaker.flag)
-			return finish(iter.breaker.err, iter.breaker.data, iter);
+		if (_iter.breaker.flag) 
+			return finish(_iter.breaker.err, _iter.breaker.data, _iter);
 		
-		if (nextPart.done) return finish(null, nextPart.value);
-			
-		return callAsync(nextPart.value, iter._loop);
+		if (nextPart.done) 
+			return finish(null, nextPart.value, _iter);
+		
+		return callAsync(nextPart.value, _iter._loop);
 	};
-	iter._loop();
+	_iter._loop();
 	
-	return iter;
+	return _iter;
 }
